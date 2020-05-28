bdbe64dcb74b9d2e8e4eefc45a7a5f0ebc951ed9
Erik Weibust <erik@weibust.net>
2020-05-27 21:09:42 -0500

    finished building tetrominoes

--- app.js
+++ app.js
@@ -1,4 +1,43 @@
 document.addEventListener('DOMContentLoaded', () => {
+   const grid = document.querySelector('.grid')
+   let squares = Array.from(document.querySelectorAll('.grid div'))
+   const ScoreDisplay = document.querySelector('#score')
+   const StartBtn = document.querySelector('#start-button')
+   const width = 10
 
+   // The Tetrominoes
+   const lTetromino = [
+      [1, width+1, width*2+1, 2], // B1, B2, B3, C1; these are indexs in one long array
+      [width, width+1, width+2, width*2+2],
+      [1, width+1, width*2+1, width*2],
+      [width, width*2, width*2+1, width*2+2]
+   ]
 
+   const sTetromino = [
+      [width+1, width+2, width*2, width*2+1],
+      [0, width, width+1, width*2+1],
+      [width+1, width+2, width*2, width*2+1],
+      [0, width, width+1, width*2+1]
+   ]
+
+   const tTetromino = [
+      [1, width, width+1, width+2],
+      [1, width+1, width+2, width*2+1],
+      [width, width+1, width+2, width*2+1],
+      [1, width, width+1, width*2+1]
+   ]
+
+   const boxTetromino = [
+      [0, 1, width, width+1],
+      [0, 1, width, width+1],
+      [0, 1, width, width+1],
+      [0, 1, width, width+1]
+   ]
+
+   const lineTetromino = [
+      [1, width+1, width*2+1, width*3+1],
+      [width, width+1, width+2, width+3],
+      [1, width+1, width*2+1, width*3+1],
+      [width, width+1, width+2, width+3]
+   ]
 })
