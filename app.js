301b97f2693a5f035f426e170012cf144ded9624
Erik Weibust <erik@weibust.net>
2020-05-28 18:43:14 -0500

    get the tetrominoes moving down the screen

--- app.js
+++ app.js
@@ -69,5 +69,27 @@ document.addEventListener('DOMContentLoaded', () => {
       })
    }
 
-   draw()
+   //draw()
+
+   timerId = setInterval(moveDown, 1000) // call moveDown() every 1000ms, or 1sec
+
+   // move down function
+   function moveDown() {
+      undraw()
+      currentPosition += width
+      draw()
+      freeze()
+   }
+
+   // freeze function
+   function freeze() {
+      if(current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
+         current.forEach(index => squares[currentPosition + index].classList.add('taken'))
+         // start a new tetromino falling
+         random = Math.floor(Math.random() * theTetrominoes.length)
+         current = theTetrominoes[random][currentRotation]
+         currentPosition = 4
+         draw()
+      }
+   }
 })
