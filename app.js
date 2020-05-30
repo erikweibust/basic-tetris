e3466a02b0198e5e2899ea923e898d7e5794d3ce
Erik Weibust <erik@weibust.net>
2020-05-29 23:42:15 -0500

    got the preview next tetromino working, sort of.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
    const ScoreDisplay = document.querySelector('#score')
    const StartBtn = document.querySelector('#start-button')
    const width = 10
+   let nextRandom = 0
 
    // The Tetrominoes
    const lTetromino = [
@@ -101,10 +102,12 @@ document.addEventListener('DOMContentLoaded', () => {
       if(current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
          current.forEach(index => squares[currentPosition + index].classList.add('taken'))
          // start a new tetromino falling
+         random = nextRandom
          random = Math.floor(Math.random() * theTetrominoes.length)
          current = theTetrominoes[random][currentRotation]
          currentPosition = 4
          draw()
+         displayShape()
       }
    }
 
@@ -148,6 +151,29 @@ document.addEventListener('DOMContentLoaded', () => {
       draw()
    }
 
+   // show up-next tetromino in mini-grid display
+   const displaySquares = document.querySelectorAll('.mini-grid div')
+   const displayWidth = 4
+   let displayIndex = 0
 
+   // the Tetrominos without rotations
+   const upNextTetrominoes = [
+      [1, displayWidth+1, displayWidth*2+1, 2], // lTetromino
+      [0, displayWidth, displayWidth+1, displayWidth*2+1], // zTetromino
+      [1, displayWidth, displayWidth+1, displayWidth+2], // tTetromino
+      [0, 1, displayWidth, displayWidth+1], // oTetromino
+      [1, displayWidth+1, displayWidth*2+1, displayWidth*3+1] //iTetromino
+   ]
+
+   // display the shape in the mini-grid display
+   function displayShape() {
+      // remove any trace of a tetromino from the entire grid
+      displaySquares.forEach(square => {
+         square.classList.remove('tetromino')
+      })
+      upNextTetrominoes[nextRandom].forEach( index => {
+         displaySquares[displayIndex + index].classList.add('tetromino')
+      })
+   }
 
 })
