b95d16ef3950fd182f2db9f7e7e202abb133a09f
Erik Weibust <erik@weibust.net>
2020-05-31 23:16:15 -0500

    have the rows removing when full

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
    const width = 10
    let nextRandom = 0
    let timerId
+   let score = 0
 
    // The Tetrominoes
    const lTetromino = [
@@ -107,6 +108,7 @@ document.addEventListener('DOMContentLoaded', () => {
          currentPosition = 4
          draw()
          displayShape()
+         addScore()
       }
    }
 
@@ -188,4 +190,24 @@ document.addEventListener('DOMContentLoaded', () => {
       }
    })
 
+   // add score
+   function addScore() {
+      for (let i = 0; i < 199; i += width) {
+         const row = [i, i+1, i+2, i+3, i+4, i+5, i+6, i+7, i+8, i+9]
+         
+         if (row.every(index => squares[index].classList.contains('taken'))) {
+            score += 10
+            scoreDisplay.innerHTML = score
+            row.forEach( index => {
+               squares[index].classList.remove('taken')
+               squares[index].classList.remove('tetromino')
+            })
+            const squaresRemoved = squares.splice(i, width)
+            //console.log(squaresRemoved)
+            squares = squaresRemoved.concat(squares)
+            squares.forEach(cell => grid.appendChild(cell))
+               
+            }
+         }
+      }
 })
